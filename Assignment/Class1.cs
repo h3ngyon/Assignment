9b2d80a50fd5273fd3f18ac563d65b1f32fed01b
h3ngyon <s10268172@connect.np.edu.sg>
2025-01-18 23:09:01 +0800

    check class tostring()

--- Assignment/Class1.cs
+++ Assignment/Class1.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace Assignment
 {
-    class Person
+  
+    class BoardingGate
     {
-        private string index;
+        private string gateName;
+        public string GateName { get;set; }
+
+        private bool supportsCFFT;
+        public bool SupportsCFFT {  get; set; }
+
+        private bool supportsDDJB;
+        public bool SupportsDDJB { get; set; }
+
+        private bool supportsLWTT;
+        public bool SupportsLWTT { get; set; }
+
+        // public Flight flight { get; set; }
+
+
+        
     }
 }
---
+++ Assignment/masterclass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    abstract class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin; 
+            Destination = destination;
+            ExpectedTime = expectedTime; 
+            Status = status;
+        }
+        public abstract double CalculateFees(); public override string ToString()
+        {
+            return $"Fight No: {FlightNumber:-10} Origin: {Origin:-20} Destination: {Destination:-20} Expected Time: {ExpectedTime:-} Status: {Status}";
+        }
+    }
+    class CFFTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestfee) : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = requestfee;
+        }
+        public override double CalculateFees()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\tRequest Fee: " + RequestFee;
+        }
+    }
+    class DDJBFlight : Flight
+    {
+        public double RequestFee { get; set; }
+        public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestfee) : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = requestfee;
+        }
+        public override double CalculateFees()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\tRequest Fee: " + RequestFee;
+        }
+    }
+
+}
