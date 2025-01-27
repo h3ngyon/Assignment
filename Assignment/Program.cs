e25316fd443f3c6dfba0b787090165cc1087fa8d
h3ngyon <s10268172@connect.np.edu.sg>
2025-01-27 10:06:24 +0800

    Initialized Terminal

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,59 +1,16 @@
 ﻿using Assignment;
 using System.ComponentModel.Design;
 
+// Create Dictionaries, Terminal
+Terminal T5 = new Terminal("Terminal 5", new Dictionary<string, Airline>(), new Dictionary<string, BoardingGate>(), new Dictionary<string, double>());
+Dictionary<string, Airline> airline_dict = new Dictionary<string, Airline>();
+Dictionary<string, BoardingGate> boarding_gate_dict = new Dictionary<string, BoardingGate>();
+Dictionary<string, BoardingGate> gatesdict = new Dictionary<string, BoardingGate>();
+
+
 // Basic Feature 1.
 
 // Key for airline_dict will use airline code "SQ"/"MH"
-Dictionary<string,Airline> airline_dict = new Dictionary<string,Airline>();
-using (StreamReader sr = new StreamReader("airlines.csv"))
-{
-    sr.ReadLine();
-    string line;
-    while ((line= sr.ReadLine()) != null)
-    {
-        string[] airline_array = line.Split(",");
-        Airline new_airline = new Airline(airline_array[0], airline_array[1]);
-        // Key for Airline_dict will be airline code
-        airline_dict.Add(airline_array[1], new_airline);
-    }
-}
-
-// Create Boarding Gates dictionary
-Dictionary<string,BoardingGate> boarding_gate_dict = new Dictionary<string,BoardingGate>();
-using (StreamReader sr = new StreamReader("boardinggates.csv"))
-{
-    sr.ReadLine();
-    string line;
-
-    while ((line = sr.ReadLine()) != null)
-    {
-        string[] boarding_gate_array = line.Split(",");
-        BoardingGate boardingGate = new BoardingGate(boarding_gate_array[0]);
-
-        // Supports DDJB
-        if (boarding_gate_array[1] == "True") 
-        { boardingGate.SupportsDDJB = true; }
-
-        // Supports CFFT
-        if (boarding_gate_array[2] == "True") 
-        { boardingGate.SupportsCFFT = true;}
-
-        // Supports LWTT
-        if (boarding_gate_array[3] == "True")
-        {
-            boardingGate.SupportsLWTT = true;
-        }
-
-        // Add new BoardingGate object to dict
-        boarding_gate_dict.Add(boarding_gate_array[0], boardingGate);
-    }
-}
-
-// Basic Feature 2
-// Load Flights from flights.csv
-
-Dictionary<string, Airline> airlinedict = new Dictionary<string, Airline>();
-Dictionary<string, BoardingGate> gatesdict = new Dictionary<string, BoardingGate>();
 
 using (StreamReader sr = new StreamReader("airlines.csv"))
 {
@@ -61,24 +18,24 @@ using (StreamReader sr = new StreamReader("airlines.csv"))
     if (s != null)
     {
         string[] heading = s.Split(',');
-    }
+    } 
     while ((s = sr.ReadLine()) != null)
     {
         string[] airlines = s.Split(",");
         string airlinename = airlines[0];
         string airlinecode = airlines[1];
         Airline airline = new Airline(airlinename, airlinecode);
-        airlinedict.Add(airline.Name, airline);
+        airline.Flights = new Dictionary<string, Flight>();
+
+        airline_dict.Add(airline.Name, airline);
+        T5.AddAirline(airline);
     }
 }
 
 using (StreamReader sr = new StreamReader("boardinggates.csv"))
 {
     string? s = sr.ReadLine();
-    if (s != null)
-    {
-        string[] heading = s.Split(",");
-    }
+        
     while ((s = sr.ReadLine()) != null)
     {
         string[] gates = s.Split(",");
@@ -97,5 +54,52 @@ using (StreamReader sr = new StreamReader("boardinggates.csv"))
             boardinggate.SupportsLWTT = true;
         }
         gatesdict.Add(boardinggate.GateName, boardinggate);
+        T5.AddBoardingGate(boardinggate);
     }
 }
+
+// Basic Feature 2
+// Load Flights from flights.csv
+Dictionary<string, Flight> flight_dict = new Dictionary<string, Flight>();
+using (StreamReader sr = new StreamReader("flights.csv"))
+{
+    sr.ReadLine();
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        Flight flight;
+        string[] data = line.Split(",");
+
+        if (data[4] == "CFFT")
+        {
+            flight = new CFFTFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]));
+        }
+        else if (data[4] == "LWTT")
+        {
+            flight = new LWTTFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]));
+        }
+        else if (data[4] == "DDJB")
+        {
+            flight = new DDJBFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]));
+        }
+        else
+        {
+            flight = new NORMFlight(data[0], data[1], data[2], Convert.ToDateTime(data[3]));
+        }
+        flight_dict.Add(flight.FlightNumber, flight);
+
+        string airlineCode = flight.FlightNumber.Substring(0, 2);
+        if (airline_dict.ContainsKey(airlineCode))
+        {
+
+            airline_dict[airlineCode].Flights.Add(flight.FlightNumber, flight);
+        }
+
+    }
+}
+
+//Initialize Terminal, T5, properties(dictionaries)
+T5.Airlines = airline_dict;
+T5.BoardingGates = boarding_gate_dict;
+T5.Flights = flight_dict;
